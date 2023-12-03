36957932b6cf7bfc4cc69956fc76b8628ef8917f
VIPon4ik <umniyd@gmail.com>
2023-12-03 22:03:42 +0200

    5.6.12 and removeed LS

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -2,15 +2,13 @@ import { createSlice } from "@reduxjs/toolkit";
 
 export const contactsSlice = createSlice({
   name: 'contacts',
-  initialState: JSON.parse(localStorage.getItem('contacts')) ?? [],
+  initialState: [],
   reducers: {
     addContact (state, action) {
       state.push(action.payload)
-      localStorage.setItem('contacts', JSON.stringify(state))
     },
     deleteContact (state, action) {
       const filtred =  state.filter((contact) => contact.id !== action.payload);
-      localStorage.setItem('contacts', JSON.stringify(filtred));
       return filtred;
     }
   }
