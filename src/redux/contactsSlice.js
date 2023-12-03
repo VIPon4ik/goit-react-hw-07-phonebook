c6fc1e5e8208d5511d6e122786decc37e058dbd1
VIPon4ik <umniyd@gmail.com>
2023-12-03 22:42:57 +0200

    Done task

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,17 +1,16 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 
 export const contactsSlice = createSlice({
   name: 'contacts',
   initialState: [],
   reducers: {
-    addContact (state, action) {
-      state.push(action.payload)
+    addContact(state, action) {
+      state.push(action.payload);
     },
-    deleteContact (state, action) {
-      return state.filter((contact) => contact.id !== action.payload);
-  
-    }
-  }
-})
+    deleteContact(state, action) {
+      return state.filter(contact => contact.id !== action.payload);
+    },
+  },
+});
 
 export const { addContact, deleteContact } = contactsSlice.actions;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,24 +1,39 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import { contactsSlice } from './contactsSlice';
 import { filterSlice } from './filterSlice';
-import { persistStore, persistReducer } from 'redux-persist';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
 const persistConfig = {
   key: 'contacts',
   storage,
+  whitelist: ['contacts'],
 };
 
-const persistedContactsReducer = persistReducer(
-  persistConfig,
-  contactsSlice.reducer
-);
+const reducer = combineReducers({
+  contacts: contactsSlice.reducer,
+  filter: filterSlice.reducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, reducer);
 
 export const store = configureStore({
-  reducer: {
-    contacts: persistedContactsReducer,
-    filter: filterSlice.reducer,
-  },
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export const persistor = persistStore(store);
